d40c9a2a79ff59d6bd4673646ae63ef69ad11e09
Fathima Nizar <fathimaen@gmail.com>
2022-07-19 23:14:56 +0530

    new app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { ContactComponent } from './contact/contact.component';
 import { LandingComponent } from './landing/landing.component';
 import { SamplesComponent } from './samples/samples.component';
 
-const routes: Routes = [{path:'landing',component:LandingComponent},{path:'about',component:AboutComponent},{path:'samples',component:SamplesComponent},{path:'contact',component:ContactComponent}];
+const routes: Routes = [{path:'',component:LandingComponent},{path:'about',component:AboutComponent},{path:'samples',component:SamplesComponent},{path:'contact',component:ContactComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
